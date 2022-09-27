45cda7d4b4d24d6c9fd5ef3deb34a4ba7a83ea28
RafaelTeniente <rafaelteniente@gmail.com>
2022-09-27 20:20:00 +0200

    DWCC

--- Practica3Ej2/js/js.js
+++ Practica3Ej2/js/js.js
@@ -4,11 +4,11 @@ window.onload = function () {
     const abrir = document.getElementById('abrir');
     const cerrar = document.getElementById('cerrar');
     let ventana;
-    const botonVentana = document.getElementById('botonVentana');
+
 
     abrir.addEventListener('click', abrirVentana);
     cerrar.addEventListener('click', cerrarVentana);
-    botonVentana.addEventListener('click', () => { window.close() });
+
 
     function abrirVentana() {
         let opciones = `width=${entrada1.value},height=${entrada2.value}`;
