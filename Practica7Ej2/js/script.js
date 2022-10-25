051e84e401039f69c0aa5d47ed7da8f2e54489c2
RafaelTeniente <rafaelteniente@gmail.com>
2022-10-25 20:57:07 +0200

    DWCC

--- Practica7Ej2/js/script.js
+++ Practica7Ej2/js/script.js
@@ -1,22 +1,36 @@
 window.onload = function () {
     const paragraph = document.getElementById('paragraph');
-    const button = document.getElementById('button');
+    const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
     let boolean = true;
+    let disabled = false;
 
     function appearance() {
         if (boolean) {
             paragraph.style.display = 'block';
-            button.innerText = 'Esconder';
+            button1.innerText = 'Esconder';
             boolean = false;
 
         } else {
             paragraph.style.display = 'none';
-            button.innerText = 'Mostrar';
+            button1.innerText = 'Mostrar';
             boolean = true;
         }
     }
 
+    function disable() {
+        if (disabled) {
+            button1.removeAttribute('disabled');
+            disabled = false;
+
+        } else {
+            button1.setAttribute('disabled', '');
+            disabled = true;
+        }
+    }
+
     appearance();
-    button.addEventListener('click', appearance)
+    button1.addEventListener('click', appearance)
+    button2.addEventListener('click', disable);
 
 }
---
+++ Practica7Ej3/js/script.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    const button1 = document.getElementById('button1');
+    const textarea = document.getElementById('textarea');
+    const events = ['mousedown', 'mouseup', 'click', 'contextmenu', 'dblclick', 'mouseover'];
+
+    addEvents();
+
+    function writeTextarea(element) {
+        textarea.value += `${element}\n`;
+    }
+
+    //caso 1
+    function addEvents() {
+        for (element of events) {
+            button1.addEventListener(element, () => { writeTextarea(element) });
+            console.log(element);
+        }
+    }
+
+
+    //caso 2
+    // function addEvents() {
+    //     for (let i = 0; i < events.length; i++) {
+    //         button1.addEventListener(events[i], () => { writeTextarea(events[i]) });
+    //         console.log(events[i]);
+    //     }
+    // }
+
+
+    //caso 3
+    // events.forEach(e => button1.addEventListener(e, () => { writeTextarea(e) }))
+
+}
